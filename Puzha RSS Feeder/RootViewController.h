3013ed1c4515a3cf5c3671ced92594f2283beaca
Sony Theakanath <stheakanath@gmail.com>
2013-08-08 11:12:16 -0700

    Adding all files

--- Puzha RSS Feeder/RootViewController.h
+++ Puzha RSS Feeder/RootViewController.h
@@ -8,9 +8,11 @@
 
 #import <UIKit/UIKit.h>
 
-@interface RootViewController : UITableViewController {
+@interface RootViewController : UITableViewController { IBOutlet UITableView * newsTable; UIActivityIndicatorView * activityIndicator; CGSize cellSize; NSXMLParser * rssParser; NSMutableArray * stories; 
+    // a temporary item; added to the "stories" array one at a time, and cleared for the next one NSMutableDictionary * item; 
+    // it parses through the document, from top to bottom... 
+    // we collect and cache each sub-element value, and then save each item to our array. 
+    // we use these to track each current item, until it's ready to be added to the "stories" array NSString * currentElement; NSMutableString * currentTitle, * currentDate, * currentSummary, * currentLink; 
+} 
 
-}
-
-
-@end
+    @end
--- Puzha RSS Feeder/RootViewController.m
+++ Puzha RSS Feeder/RootViewController.m
@@ -22,9 +22,22 @@
 
 - (void)viewDidAppear:(BOOL)animated
 {
-    [super viewDidAppear:animated];
+    [super viewDidAppear:animated]; if ([stories count] == 0) { NSString * path = @"http://rss.cnn.com/rss/cnn_topstories.rss"; [self parseXMLFileAtURL:path]; } cellSize = CGSizeMake([newsTable bounds].size.width, 60); 
 }
 
+- (void)parseXMLFileAtURL:(NSString *)URL { stories = [[NSMutableArray alloc] init]; 
+    //you must then convert the path to a proper NSURL or it won't work 
+    NSURL *xmlURL = [NSURL URLWithString:URL]; 
+    // here, for some reason you have to use NSClassFromString when trying to alloc NSXMLParser, otherwise you will get an object not found error
+    // this may be necessary only for the toolchain 
+    rssParser = [[NSXMLParser alloc] initWithContentsOfURL:xmlURL]; 
+    // Set self as the delegate of the parser so that it will receive the parser delegate methods callbacks. 
+    [rssParser setDelegate:self]; 
+    // Depending on the XML document you're parsing, you may want to enable these features of NSXMLParser. 
+    [rssParser setShouldProcessNamespaces:NO]; [rssParser setShouldReportNamespacePrefixes:NO]; [rssParser setShouldResolveExternalEntities:NO]; [rssParser parse]; }
+
+- (void)parserDidStartDocument:(NSXMLParser *)parser { NSLog(@"found file and started parsing"); } - (void)parser:(NSXMLParser *)parser parseErrorOccurred:(NSError *)parseError { NSString * errorString = [NSString stringWithFormat:@"Unable to download story feed from web site (Error code %i )", [parseError code]]; NSLog(@"error parsing XML: %@", errorString); UIAlertView * errorAlert = [[UIAlertView alloc] initWithTitle:@"Error loading content" message:errorString delegate:self cancelButtonTitle:@"OK" otherButtonTitles:nil]; [errorAlert show]; } - (void)parser:(NSXMLParser *)parser didStartElement:(NSString *)elementName namespaceURI:(NSString *)namespaceURI qualifiedName:(NSString *)qName attributes:(NSDictionary *)attributeDict{(@"found this element: %@", elementName); currentElement = [elementName copy]; if ([elementName isEqualToString:@"item"]) { // clear out our story item caches... item = [[NSMutableDictionary alloc] init]; currentTitle = [[NSMutableString alloc] init]; currentDate = [[NSMutableString alloc] init]; currentSummary = [[NSMutableString alloc] init]; currentLink = [[NSMutableString alloc] init]; } } - (void)parser:(NSXMLParser *)parser didEndElement:(NSString *)elementName namespaceURI:(NSString *)namespaceURI qualifiedName:(NSString *)qName{ //NSLog(@"ended element: %@", elementName); if ([elementName isEqualToString:@"item"]) { // save values to an item, then store that item into the array... [item setObject:currentTitle forKey:@"title"]; [item setObject:currentLink forKey:@"link"]; [item setObject:currentSummary forKey:@"summary"]; [item setObject:currentDate forKey:@"date"]; [stories addObject:[item copy]]; NSLog(@"adding story: %@", currentTitle); } } - (void)parser:(NSXMLParser *)parser foundCharacters:(NSString *)string{ //NSLog(@"found characters: %@", string); // save the characters for the current item... if ([currentElement isEqualToString:@"title"]) { [currentTitle appendString:string]; } else if ([currentElement isEqualToString:@"link"]) { [currentLink appendString:string]; } else if ([currentElement isEqualToString:@"description"]) { [currentSummary appendString:string]; } else if ([currentElement isEqualToString:@"pubDate"]) { [currentDate appendString:string]; } } - (void)parserDidEndDocument:(NSXMLParser *)parser { [activityIndicator stopAnimating]; [activityIndicator removeFromSuperview]; NSLog(@"all done!"); NSLog(@"stories array has %d items", [stories count]); [newsTable reloadData]; 
+}
 - (void)viewWillDisappear:(BOOL)animated
 {
 	[super viewWillDisappear:animated];
@@ -51,23 +64,20 @@
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
-    return 0;
+    return [stories count];
 }
 
 // Customize the appearance of table view cells.
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
-    static NSString *CellIdentifier = @"Cell";
+    static NSString *MyIdentifier = @"MyIdentifier";
     
-    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:MyIdentifier];
     if (cell == nil) {
-        cell = [[[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:CellIdentifier] autorelease];
-    }
-
+        cell = [[[UITableViewCell alloc] initWithFrame:CGRectZero reuseIdentifier: MyIdentifier] autorelease]; }
+    // Set up the cell
+    int storyIndex = [indexPath indexAtPosition: [indexPath length] - 1]; [cell setText:[[stories objectAtIndex: storyIndex] objectForKey: @"title"]]; return cell; }
     // Configure the cell.
-    return cell;
-}
-
 /*
 // Override to support conditional editing of the table view.
 - (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath
